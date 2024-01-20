e56a157e8adbfe2cb8655b45ebaa058ee5317243
DiorEtMoi <110081209+DiorEtMoi@users.noreply.github.com>
2024-01-20 13:25:47 +0700

    update api for user and add constants to reuse

---
+++ constants/messageError.js
@@ -0,0 +1,7 @@
+const MessageError = {
+    email : "Invalid email format !",
+    phoneNumber : "Invalid phonenumber format, phonenumber lengths must be 10 !",
+    inputNotContainSpecial : " can not contain special character !",
+    idExist : "ID already exist !",
+}
+export default MessageError;
--- controllers/user.js
+++ controllers/user.js
@@ -1,3 +1,5 @@
+import jwt from 'jsonwebtoken'
+import MessageError from "../constants/messageError.js";
 import User from "../models/user.js";
 import userRepository from "../repositories/user.js";
 import Validator from "../validator/validator.js"
@@ -8,19 +10,19 @@ const userController = {
             if(!Validator.checkString(username)){
                 return resp.status(400).json({
                     success : false,
-                    error : "username can not contain special character !"
+                    error : "username " + MessageError.inputNotContainSpecial 
                 })
             }
             if(!Validator.checkEmail(email)){
                 return resp.status(400).json({
                     success : false,
-                    error : "Invalid email format !"
+                    error : MessageError.email
                 });
             }
             if(!Validator.isValidPhoneNumber(phoneNumber)){
                 return resp.status(400).json({
                     success : false,
-                    error : "Invalid phonenumber format, phonenumber lengths must be 10 !"
+                    error : MessageError.phoneNumber
                 });
             }
             const user = await User.findOne({email});
@@ -40,6 +42,43 @@ const userController = {
                 success : false,
                 error : error.message
             })
+        }
+    },
+    loginAccount : async (req,resp) => {
+        try {
+            const {email} = req.body;
+            if(!Validator.checkEmail(email)){
+                return resp.status(400).json({
+                    success : false,
+                    error : MessageError.email
+                })
+            }
+            const user = await userRepository.loginAccount(req.body);
+            if(!user){
+                return resp.status(400).json({
+                    success : false,
+                    error : "Email, Password not correctly !"
+                });
+            }
+            const payload = {
+                user_id : user._id,
+                email : user.email,
+                user_name : user.username,
+                role : user.role_id.role_name
+            }
+            const token = jwt.sign(payload,process.env.SECRET_KEY,{
+                expiresIn : "365d"
+            });
+            return resp.status(200).json({
+                success : true,
+                token,
+                message : "Login successfully !"
+            })
+        } catch (error) {
+            return resp.status(400).json({
+                success : false,
+                error : error.message
+            });
         }
     }
 }
