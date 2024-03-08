0bb686396c9a4842c9f623d2f7cf01b8f35bcc58
QuanHH <huyquanwork95@gmail.com>
2024-03-08 12:41:28 +0700

    update location

--- repositories/location.js
+++ repositories/location.js
@@ -1,12 +1,23 @@
 import Location from "../models/location.js"
 const locationRepository = {
-    createLocation : async (locationInfor) => {
+    createLocation: async (locationInfor) => {
         try {
             const locationSaved = await Location.create({
-                location_name : locationInfor.location_name
+                location_name: locationInfor.location_name
             });
             return {
-                status : "OK",
+                status: "OK",
+                locationSaved
+            }
+        } catch (error) {
+            throw new Error(error);
+        }
+    },
+    findAll: async (locationInfor) => {
+        try {
+            const locationSaved = await Location.find().populate(["location_name"]);
+            return {
+                status: "Done",
                 locationSaved
             }
         } catch (error) {
